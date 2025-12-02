023a16a980944df64d18a7794df05fbdf4f2d220
0xletuss <yamiyuhiko@gmail.com>
2025-12-02 11:03:02 +0800

    new commit

--- routes/product_routes.py
+++ routes/product_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import db
-from models.product import Product, Inventory
+from models.products import Product, Inventory
 from models.user import Seller
 
 product_bp = Blueprint('product', __name__)
